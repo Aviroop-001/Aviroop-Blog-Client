b4df56f8eea6df3271d93f6750163d989389510f
Aviroop-001 <banerjeeaviroop01@gmail.com>
2022-08-21 19:46:10 +0530

    updated posts component ui

--- src/Components/Posts/Posts.jsx
+++ src/Components/Posts/Posts.jsx
@@ -1,13 +1,17 @@
 import './Posts.scss';
 import Post from '../Post/Post';
+import { Box } from '@chakra-ui/react';
 
 const Posts = ({posts}) => {
   return (
-    <div className='Posts'>
+    <Box className='Posts' display='flex'
+    justifyContent='space-evenly'
+    flexWrap='wrap'
+    margin='1rem 0.5rem'>
       {posts.map( (p) => (
         <Post key={p._id} post={p}/>
       ))}
-    </div>
+    </Box>
   )
 }
 
